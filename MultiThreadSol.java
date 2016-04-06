f4957a1afcf9246a12656cd2cfb94f46edd0bffc
Lea <for.lea@gmail.com>
2016-04-07 00:11:50 +0300

    Code was reformatted, threads are not saved now.

--- MultiThreadSol.java
+++ MultiThreadSol.java
@@ -3,14 +3,16 @@
  */
 public class MultiThreadSol implements Runnable {
     private long start, fin;
-    MultiThreadSol(long start, long fin){
-        this.start= start;
-        this.fin=fin;
+
+    MultiThreadSol(long start, long fin) {
+        this.start = start;
+        this.fin = fin;
     }
+
     @Override
     public void run() {
-        OneThreadSolution sol= new OneThreadSolution();
-        sol.solve(start,fin);
+        OneThreadSolution sol = new OneThreadSolution();
+        sol.solve(start, fin);
     }
 
 }
--- Solve.java
+++ Solve.java
@@ -5,24 +5,14 @@ public class Solve {
     public static void main(String[] args) {
         System.out.print("One Thread: ");
         OneThreadSolution sol = new OneThreadSolution();
-
-        sol.solve(123456789,987654321);
-
+        sol.solve(123456789, 987654321);
 
         System.out.print("Five Threads: ");
-       Thread[] sols= new Thread[5];
-        sols[0]= new Thread(new MultiThreadSol(123_456_789,298_765_431));
-        sols[1]= new Thread(new MultiThreadSol(312_456_789,498_765_321));
-        sols[2]= new Thread(new MultiThreadSol(512_346_789,698_754_321));
-        sols[3]= new Thread(new MultiThreadSol(712_345_689,897_654_321));
-        sols[4]= new Thread(new MultiThreadSol(912_345_678,987_654_321));
-
-        for (int i = 0; i < 5; i++) {
-            sols[i].start();
-        }
-
-
-
+        (new Thread(new MultiThreadSol(123_456_789, 298_765_431))).start();
+        (new Thread(new MultiThreadSol(312_456_789, 498_765_321))).start();
+        (new Thread(new MultiThreadSol(512_346_789, 698_754_321))).start();
+        (new Thread(new MultiThreadSol(712_345_689, 897_654_321))).start();
+        (new Thread(new MultiThreadSol(912_345_678, 987_654_321))).start();
 
     }
 }
