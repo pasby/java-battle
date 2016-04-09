48179827381657440c749bf3bc0187f62491c7a8
Gadd <for.gadd@gmail.com>
2016-04-09 10:32:16 +0300

    Adding CLI abilities

--- gadd/src/main/java/textDictionary/Run.java
+++ gadd/src/main/java/textDictionary/Run.java
@@ -4,7 +4,8 @@ import java.util.Map;
 
 public class Run {
     public static void main(String[] args) {
-        CreateDictionary cd = new CreateDictionary("/home/gadd/example_text.txt");
+        String path = args[0];
+        CreateDictionary cd = new CreateDictionary(path);
         Map<String, Integer> dict = cd.getResult();
         for (Map.Entry<String, Integer> item : dict.entrySet()) {
             System.out.println(item.getKey() + " " + item.getValue());
