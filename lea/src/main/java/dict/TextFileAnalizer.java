f094b39a070b5d28125a2506a4306d7347a9b080
Lea <for.lea@gmail.com>
2016-04-14 22:41:43 +0300

    Fixed.

--- lea/src/main/java/dict/TextFileAnalizer.java
+++ lea/src/main/java/dict/TextFileAnalizer.java
@@ -21,27 +21,27 @@ public class TextFileAnalizer {
     public void getText() {
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
-            Map<String,Integer> dict= new HashMap<String, Integer>();
+            Map<String, Integer> dict = new HashMap<String, Integer>();
             String line;
-            while((line=br.readLine())!=null){
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                String[] words=line.toLowerCase().split("\\W");
-                for(String s:words){
-                    if (dict.containsKey(s)){
-                        int val= dict.get(s).intValue()+1;
-                        dict.put(s,val);
-                    }else{
-                        dict.put(s,1);
+                String[] words = line.toLowerCase().split("\\W");
+                for (String s : words) {
+                    if (dict.containsKey(s)) {
+                        int val = dict.get(s).intValue() + 1;
+                        dict.put(s, val);
+                    } else {
+                        dict.put(s, 1);
                     }
 
                 }
             }
             br.close();
-            for(String s:dict.keySet()){
-                System.out.println(s+ " -> "+dict.get(s));
+            for (String s : dict.keySet()) {
+                System.out.println(s + " -> " + dict.get(s));
             }
         } catch (IOException e) {
-            System.out.println("Error");
+            e.printStackTrace();
             return;
         }
 
